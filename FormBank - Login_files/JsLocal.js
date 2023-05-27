dd962efd0f6bd2501234406cd36bf8b9fb509d9f
Yohannes <john.tikabo@gmail.com>
2023-05-27 09:21:53 -0400

    Update JsLocal.js

--- FormBank - Login_files/JsLocal.js
+++ FormBank - Login_files/JsLocal.js
@@ -1,11 +1,13 @@
 	function openWindowOnNewTab(){
 		//window.open('http://www.website.com/page');
 		const url = 'https://www.google.com';
+		const options = { animated: true, modal: false };
+
 		 const message = {
     url,
     options
   };
-        const options = { animated: true, modal: false };
+		
 		  window.webkit.messageHandlers.openWindow.postMessage(message);
 
 		}
